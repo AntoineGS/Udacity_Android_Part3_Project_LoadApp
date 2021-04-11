b7345fa480280f276d691205a54eaa14330717ad
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2021-04-11 10:58:54 -0400

    Add data binding, internet permission, toast when nothing is selected, URLs for all 3 radios

--- app/src/main/java/com/udacity/MainActivity.kt
+++ app/src/main/java/com/udacity/MainActivity.kt
@@ -9,13 +9,18 @@ import android.content.Intent
 import android.content.IntentFilter
 import android.net.Uri
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NotificationCompat
+import com.udacity.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.content_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private enum class DownloadType {GLIDE, PROJECT, RETROFIT}
 
     private var downloadID: Long = 0
 
@@ -23,15 +28,30 @@ class MainActivity : AppCompatActivity() {
     private lateinit var pendingIntent: PendingIntent
     private lateinit var action: NotificationCompat.Action
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         registerReceiver(receiver, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
 
         custom_button.setOnClickListener {
-            download()
+            when (binding.content.optionsRadioGroup.checkedRadioButtonId) {
+                View.NO_ID -> {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.select_download),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+                R.id.glide_radio -> download(DownloadType.GLIDE)
+                R.id.udacity_radio -> download(DownloadType.PROJECT)
+                R.id.retrofit_radio -> download(DownloadType.RETROFIT)
+            }
         }
     }
 
@@ -41,9 +61,17 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun download() {
+    private fun download(downloadType: DownloadType) {
+        val url = (
+                when (downloadType) {
+                    DownloadType.GLIDE -> GLIDE_URL
+                    DownloadType.PROJECT -> PROJECT_URL
+                    DownloadType.RETROFIT -> RETROFIT_URL
+                }
+        )
+
         val request =
-            DownloadManager.Request(Uri.parse(URL))
+            DownloadManager.Request(Uri.parse(url))
                 .setTitle(getString(R.string.app_name))
                 .setDescription(getString(R.string.app_description))
                 .setRequiresCharging(false)
@@ -56,8 +84,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val URL =
+        private const val PROJECT_URL =
             "https://github.com/udacity/nd940-c3-advanced-android-programming-project-starter/archive/master.zip"
+        private const val GLIDE_URL = "https://github.com/bumptech/glide/archive/refs/heads/master.zip"
+        private const val RETROFIT_URL = "https://github.com/square/retrofit/archive/refs/heads/master.zip"
         private const val CHANNEL_ID = "channelId"
     }
 
