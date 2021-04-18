61a01d08e249e786d412ba4960f60ab41f4d7951
Antoine Gaudreau Simard <antoineg.simard@gmail.com>
2021-04-18 15:23:23 -0400

    add motion scene, pass and handle values in detail activity

--- app/src/main/java/com/udacity/DetailActivity.kt
+++ app/src/main/java/com/udacity/DetailActivity.kt
@@ -20,9 +20,21 @@ class DetailActivity : AppCompatActivity() {
         val binding = ActivityDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
+        supportActionBar?.title = getString(R.string.detail)
 
         val notification: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-        notification.cancelAll()
+        notification.cancelAll() // If we had support for multiple types this would not be a good approach
+
+        binding.contentDetail.title.text = (
+                when (downloadType) {
+                    DownloadType.GLIDE -> getString(R.string.download_glide)
+                    DownloadType.PROJECT -> getString(R.string.download_project)
+                    DownloadType.RETROFIT -> getString(R.string.download_retrofit)
+                }
+                )
+
+        // in a production app this would obviously be translated to human readable text-based statuses
+        binding.contentDetail.status.text = status.toString()
     }
 
 }
